197cf690631a79d6f229ba7a8af6d9b43e147ac6
Christoph M. Wintersteiger <christoph@winterstiger.at>
2021-04-02 22:46:16 +0000

    Sleep

--- sleep.h
+++ sleep.h
@@ -5,14 +5,17 @@
 #define _SLEEP_H_
 
 #include <time.h>
+#include <errno.h>
+#include <stdint.h>
+#include <sys/time.h>
 
 #include <algorithm>
 
 inline void sleep_ms(unsigned ms)
 {
-  struct timespec duration = {0}, remaining = {0};
-  duration.tv_sec = 0;
-  duration.tv_nsec = ms * 1000000L;
+  struct timespec ts = {0};
+  ts.tv_sec = ms / 1000ul;
+  ts.tv_nsec = (ms % 1000ul) * 1000000ul;
 
   struct timespec *pdur = &duration, *prem = &remaining;
   while (nanosleep(pdur, prem) == -1)
@@ -21,20 +24,32 @@ inline void sleep_ms(unsigned ms)
 
 inline void sleep_us(unsigned us)
 {
-  struct timespec duration = {0}, remaining = {0};
-  duration.tv_sec = 0;
-  duration.tv_nsec = us * 1000L;
+  if (us < 100) {
+    struct timeval tNow, tulong, tEnd;
 
-  struct timespec *pdur = &duration, *prem = &remaining;
-  while (nanosleep(pdur, prem) == -1)
-    std::swap(pdur, prem);
+    gettimeofday(&tNow, NULL);
+    tulong.tv_sec  = us / 1000000;
+    tulong.tv_usec = us % 1000000;
+    timeradd(&tNow, &tulong, &tEnd);
+
+    while (timercmp(&tNow, &tEnd, <))
+      gettimeofday(&tNow, NULL);
+  }
+  else {
+    struct timespec ts = {0};
+    ts.tv_sec = us / 1000000ul;
+    ts.tv_nsec = (us % 1000000ul) * 1000ul;
+
+    while (nanosleep(&ts, &ts) == -1 && errno == EINTR)
+      ;
+  }
 }
 
 inline void sleep_ns(unsigned ns)
 {
-  struct timespec duration = {0}, remaining = {0};
-  duration.tv_sec = 0;
-  duration.tv_nsec = ns * 1000L;
+  struct timespec ts = {0};
+  ts.tv_sec = ns / 1000000000ul;
+  ts.tv_nsec = ns % 1000000000ul;
 
   struct timespec *pdur = &duration, *prem = &remaining;
   while (nanosleep(pdur, prem) == -1)
